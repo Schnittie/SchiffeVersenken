7a2012e30358e35e2e0cce3958c0eecbab6039e2
laurenz <i22032@hb.dhbw-stuttgart.de>
2024-01-12 16:00:21 +0100

    Persistance WIP

--- Persistance.cpp
+++ Persistance.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "Persistance.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void Persistance::saveString(std::string fileName, std::string gameString) {
+    std::ofstream outFile(fileName);
+    if (outFile.is_open()) {
+        outFile << gameString;
+        outFile.close();
+        std::cout << "Game successfully saved to " << fileName << std::endl;
+    } else {
+        std::cerr << "Error opening file: " << fileName << std::endl;
+    }
+}
+
+void
+Persistance::saveGame(std::unique_ptr<Board> playerBoard, std::unique_ptr<Board> opponentBoard, int opponentLevel) {
+    std::string saveGameString;
+    saveGameString.append(turnBoardIntoString(std::move(playerBoard->createCopy())));
+    saveGameString.append("\n");
+    saveGameString.append(turnBoardIntoString(std::move(opponentBoard->createCopy())));
+    saveGameString.append("\n");
+    saveGameString.append(std::to_string(opponentLevel));
+}
+
+std::string Persistance::turnBoardIntoString(std::unique_ptr<Board>) {
+    std::string guessString;
+    std::string shipString;
+}
--- Persistance.h
+++ Persistance.h
@@ -6,7 +6,18 @@
 #define SCHIFFEVERSENKEN_PROJEKT__PERSISTANCE_H
 
 
+#include <string>
+#include <memory>
+#include "Board.h"
+
+class Board;
 class Persistance {
+public:
+    static void saveGame(std::unique_ptr<Board>,std::unique_ptr<Board>,int);
+private:
+    static void saveString(std::string, std::string );
+    static std::string turnBoardIntoString(std::unique_ptr<Board>);
+
 //TODO: Hier kommt das Speichern und laden von Spielständen hin, das einzige was wirklich gespeichert werden muss ist wie das Board aussieht, da immer wenn das Speichern möglich ist sowieso der Spieler dran ist
 //TODO: evtl. falls wir den Opponent fancy machen müssten wir noch speichern gegen welche Art von Opponent gespielt wird
 };
