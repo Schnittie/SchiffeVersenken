7943e086bb8aaa7db5d08d409c617dec93dae09c
laurenz <i22032@hb.dhbw-stuttgart.de>
2024-01-18 10:49:48 +0100

    English instructions

--- Persistance.h
+++ Persistance.h
@@ -31,10 +31,6 @@ private:
 
     static std::string turnBoardIntoString(std::unique_ptr<Board>);
 
-//TODO: Hier kommt das Speichern und laden von Spielständen hin, das einzige was wirklich gespeichert werden muss ist wie das Board aussieht, da immer wenn das Speichern möglich ist sowieso der Spieler dran ist
-//TODO: evtl. falls wir den Opponent fancy machen müssten wir noch speichern gegen welche Art von Opponent gespielt wird
-
-
     static int getIntFromGuessStatus(GuessStatus guessStatus);
     static void tryCreatingSaveDirectory();
     static GameState getGameStateFromFile(std::string filename);
